6bb23d7b1780268a21d00083f4d7e8a2425cc03c
ArLoTeTxU <arlotetxu@pm.me>
2023-06-19 19:53:47 +0200

    Firs commit

---
+++ learning/initial_pipe.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initial_pipe.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jflorido <jflorido@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/19 18:43:32 by jflorido          #+#    #+#             */
+/*   Updated: 2023/06/19 19:22:51 by jflorido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+#define READ_END		0
+#define WRITE_END	1
+
+int	main(int argc, char **argv)
+{
+	int fd1[2], fd2[2];
+	int status, pid;
+
+
+}
